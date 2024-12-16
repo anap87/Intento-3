0a63d333eba18c02c9886b9eedbe6a19b6069422
anap87 <apor2209@gmail.com>
2024-12-16 07:45:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function mostrarSecciones(seccionID) {
     const secciones = document.querySelectorAll('.seccion');
     // Primero ocultamos todas las secciones
-    secciones.forEach(seccion => {
-        seccion.classList.add('ocultar'); // Se añaden todas las secciones a la clase 'ocultar'
-    });
+    secciones.forEach(seccion => seccion.classList.remove('activa'));
 
     // Buscamos la sección que se debe mostrar
     const seccionSeleccionada = document.getElementById(seccionID);
     if (seccionSeleccionada) {
-        // Eliminamos la clase 'ocultar' de la sección seleccionada
-        seccionSeleccionada.classList.remove('ocultar');
+        // Agregamos la clase activa para mostrarla
+        seccionSeleccionada.classList.add('activa');
     } else {
         console.log("Sección no encontrada: " + seccionID); // Verifica si la sección no existe
     }
@@ -21,9 +19,3 @@ document.getElementById('menu-btn').addEventListener('click', function() {
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
 
-
-// Evento para mostrar/ocultar el menú flotante
-document.getElementById('menu-btn').addEventListener('click', function() {
-    const menu = document.getElementById('menu-flotante');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-});
