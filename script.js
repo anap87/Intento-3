4c72da0b4bf021ae400fe1623213d7a773b39ba7
anap87 <apor2209@gmail.com>
2025-01-08 05:23:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ document.getElementById('menu-btn').addEventListener('click', function() {
     const menu = document.getElementById('menu-flotante');
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
+
+// Menu pagina interna quienes somos
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menu = document.querySelector(".menu2"); // Aquí seleccionamos el menú con la clase menu2
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) { // Cambia 100 según el punto de scroll donde desees el cambio
+            menu.classList.add("menu-scroll");
+        } else {
+            menu.classList.remove("menu-scroll");
+        }
+    });
+});
