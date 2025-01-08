2cffc9a9c2682facbbf9553af1bf82a7e103d2e4
anap87 <apor2209@gmail.com>
2025-01-08 05:58:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,14 +22,17 @@ document.getElementById('menu-btn').addEventListener('click', function() {
 // Menu pagina interna quienes somos
 
 // Solo aplica el cambio de fondo si body tiene la clase pagina-especial
-if (document.body.classList.contains('pagina-especial')) {
-    window.addEventListener('scroll', function () {
-        const menu = document.querySelector('.menu2');
-        if (window.scrollY > 50) {
-            menu.classList.add('scrolled'); // Aplica color de fondo al hacer scroll
-        } else {
-            menu.classList.remove('scrolled'); // Restaura el fondo transparente
-        }
-    });
+if (window.location.pathname === '/quienes-somos.html') {
+    // Solo ejecutamos el código si estamos en la página "quienes-somos.html"
+    if (document.body.classList.contains('pagina-especial')) {
+        window.addEventListener('scroll', function () {
+            const menu = document.querySelector('.menu2');
+            if (window.scrollY > 50) {
+                menu.classList.add('scrolled');
+            } else {
+                menu.classList.remove('scrolled');
+            }
+        });
+    }
 }
 
